1c28dcd224ec1cf758a0710ba6d03eaa33703f60
Venus <bisnesbrend@gmail.com>
2022-10-16 22:53:23 +0300

    Добавьте файлы проекта.

---
+++ Daily/Controllers/HomeController.cs
@@ -0,0 +1,70 @@
+﻿using Daily.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Daily.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        public IActionResult Index()
+        {
+            TaskToday tasks = new TaskToday();
+
+            return View(tasks);
+        }
+
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Daily/Models/TaskToday.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Daily.Models
+{
+    public class TaskToday
+    {
+        public string CurrentDay { get; set; }
+        public List<SingleTask> Tasks { get; set; }
+
+        public TaskToday()
+        {
+            CurrentDay = DateTime.Now.ToString("dd.MM");
+
+            Tasks = new List<SingleTask>();
+
+            for (int i = 1; i <= 30; i++)
+            {
+                Tasks.Add(new SingleTask(i, "Написать ежедневник Написать ежедневник Написать ежедневник Написать ежедневник Написать ежедневник Написать ежедневник"));
+            }
+        }
+    }
+
+    public class SingleTask
+    {
+        public int Id { get; set; }
+        public string Time { get; set; }
+        public string Task { get; set; }
+
+        public SingleTask(int id, string task)
+        {
+            Id = id;
+            Time = DateTime.Now.ToString("mm:ff");
+            Task = task;
+        }
+    }
+}
